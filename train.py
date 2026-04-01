eb3f483675d6825ab9b3cfdabbf4a715bb39dde6
Abdeladim Zine eddine <zineeddine.abdeladim@gmail.com>
2026-04-02 00:06:44 +0200

    Added feature scaling and model training loop

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from model import LinearRegressionGD
 
 np.random.seed(42)
 X = 5 * np.random.rand(100, 1) 
@@ -18,3 +19,15 @@ X_train = train_df[['Experience']].values
 y_train = train_df['Salary'].values
 X_test = test_df[['Experience']].values
 y_test = test_df['Salary'].values
+
+# Data Scaling (Standardization)
+x_mean, x_std = X_train.mean(), X_train.std()
+X_train_scaled = (X_train - x_mean) / x_std
+X_test_scaled = (X_test - x_mean) / x_std
+
+# Init
+print("\nStarting training...")
+model = LinearRegressionGD(learning_rate=0.1, n_iterations=200)
+model.fit(X_train_scaled, y_train)
+
+print(f"Training completed. Weights : {model.weights[0]:.2f}, Bias : {model.bias:.2f}")
